6a2872826a4d5b3f1800bb7a388b3530c9855e3f
Molly Ann McDonough <mcdonough.mollya@gmail.com>
2022-03-21 14:43:19 -0400

    commit tree 596c688553b3da3c90d4f6dd8fee352a2d435b52

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,12 @@
 class Show < ActiveRecord::Base
-  
+    belongs_to :network
+    has_many :characters
+    has_many :actors, through: :characters
+    def actors_list
+        actor_names = []
+        self.actors.each do |actor|
+            actor_names << actor.full_name
+        end
+        actor_names
+    end
 end
